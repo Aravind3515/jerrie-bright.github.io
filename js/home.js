25d647eef7d6d5d2e62ade40f993daa0d92477aa
Jerrin Bright <Jerriebright@gmail.com>
2020-12-22 11:31:08 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -49,3 +49,81 @@ $('#number4').jQuerySimpleCounter({end: 12,duration: 5000});
         });
   
 
+(function() {
+  // Prelodr Example
+  // ---------------
+
+  // Definition
+  const pre = prelodr()
+  
+  pre.on('shown', function() {
+    console.log('shown!')
+  })
+
+  pre.on('hidden', function() {
+    console.log('hidden!')
+  })
+  
+  // Click Event
+  const btn = document.getElementById('my-btn')
+  btn.addEventListener('click', function() {
+
+    // In action
+    pre
+      .show('Initializing...')
+      .hide(function(done) {
+        console.info('1 second delay...')
+          // Simulate 1 second delay
+        setTimeout(done, 1000)
+      })
+
+    .show('Processing...')
+      .hide(function(done) {
+        console.info('1 seconds delay...');
+        // Simulate 2 seconds delay
+        setTimeout(done, 1000)
+      })
+
+    .show('<b>Finishing...</b>').hide();
+
+  }, false)
+
+})(window)
+
+/*
+https://github.com/daybrush/scenejs
+*/
+const scene = new Scene({
+  ".container": {}
+}, {
+  selector: true,
+});
+
+const item = scene.getItem(".container");
+
+
+function move(startTime, endTime, left, top, rotate, scale) {
+  item.set({
+    [`${startTime}, ${endTime}`]: Scene.kineticFrame({
+      left: `${left}px`,
+      top: `${top}px`
+    }).set({
+      transform: {
+        rotate: `${rotate}deg`,
+        scale,
+      }
+    }),
+  });
+}
+
+move(0, 0, 90, 115, 0, 5);
+move(1, 1, 90, 115, 0, 2);
+move(2, 3, 0, 115, 0, 1);
+move(4, 4.5, -100, 0, -90, 2);
+move(5.5, 6, -52, -18, -90, 1.6);
+move(7, 7.5, 30, 45, 0, 2);
+move(8.5, 9,  10, 30, 0, 3);
+move(10, 10.5, 28, 0, 0, 2.2);
+move(11.5, 12, 50, -35, 0, 1.65);
+move(13, 13.5, 35, -70, 0, 2);
+move(14.5, 18, 0, 0, 0, 1);
